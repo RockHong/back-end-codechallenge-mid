b7d46905f560b22c32b427384cd75015da8f7c33
Zhihua Hong <hongzhihua@gmail.com>
2023-08-13 17:12:43 +0800

    init spring boot app

---
+++ src/main/java/thrive/ThriveAppApplication.java
@@ -0,0 +1,13 @@
+package thrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThriveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThriveAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thrive/ThriveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package thrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThriveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
